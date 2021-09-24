79dde784aed9aa8527bddffac32ca700f32c2df5
badarirao <badari.rao@gmail.com>
2021-09-24 15:20:51 +0900

    Upadated Fakeadapter Signed-off-by: badarirao <badari.rao@gmail.com>

--- utilities.py
+++ utilities.py
@@ -1,16 +1,23 @@
+"""Utilities file with useful classes and functions."""
+
 from datetime import datetime
-from os.path import abspath,join,exists
+from os.path import abspath, join, exists
+from pyvisa import ResourceManager, VisaIOError
 from os import makedirs
 from copy import copy
 from re import sub
+from sys import exit as exitprogram
+from MyKeithley2450 import Keithley2450
+from MyKeithley2700 import Keithley2700
+from MyAFG1022 import AFG1022 
 
 class FakeAdapter():
-    """Provides a fake adapter for debugging purposes,
-    which bounces back the command so that arbitrary values 
-    testing is possible.
-    
+    """Provides a fake adapter for debugging purposes.
+
+    Bounces back the command so that arbitrary values testing is possible.
+
     Note: Adopted from Pymeasure package
-    
+
     .. code-block:: python
 
         a = FakeAdapter()
@@ -26,30 +33,34 @@ class FakeAdapter():
     _buffer = ""
 
     def read(self):
-        """ Returns the last commands given after the
-        last read call.
-        """
+        """Return last commands given after the last read call."""
         result = copy(self._buffer)
         # Reset the buffer
         self._buffer = ""
         return result
 
     def write(self, command):
-        """ Writes the command to a buffer, so that it can
-        be read back.
-        """
+        """Write the command to a buffer, so that it can be read back."""
         self._buffer += command
 
     def __repr__(self):
+        """Return the class name as a string."""
         return "<FakeAdapter>"
-    
-    def reset(self):
-        pass
+
+    def __getattr__(self,name):  
+        """If any undefined method is called, do nothing."""
+        def method(*args):
+            pass
+        return method
+
+
 
 def unique_filename(directory, prefix='DATA', suffix='', ext='csv',
                     dated_folder=False, index=True, datetimeformat="%Y-%m-%d"):
-    """ Returns a unique filename based on the directory and prefix
-        Note: adopted from Pymeasure Package
+    """
+    Return a unique filename based on the directory and prefix.
+
+    Note: adopted from Pymeasure Package.
     """
     now = datetime.now()
     directory = abspath(directory)
@@ -66,9 +77,11 @@ def unique_filename(directory, prefix='DATA', suffix='', ext='csv',
             i += 1
             filename = "%s_%d%s.%s" % (basepath, i, suffix, ext)
     else:
-        basename = "%s%s%s.%s" % (prefix, now.strftime(datetimeformat), suffix, ext)
+        basename = "%s%s%s.%s" % (
+            prefix, now.strftime(datetimeformat), suffix, ext)
         filename = join(directory, basename)
     return filename
+
 
 def get_valid_filename(s):
     """
@@ -87,3 +100,50 @@ def get_valid_filename(s):
     """
     s = str(s).strip().replace(' ', '_')
     return sub(r'(?u)[^-\w.]', '', s)
+
+
+def checkInstrument(test = False):
+    """
+    Obtain instrument address of K2450, K2700 and function generator.
+
+    Returns: list of instrument objects
+    -------
+    Instrument objects pointing to K2450, K2700 and AFG1022
+    if test is True,
+        return FakeAdapter if any instrument is not found
+    else exit program
+    """
+    rm = ResourceManager()
+    instList = rm.list_resources()
+    k2450Addr = ''
+    k2700Addr = ''
+    AFG1022Addr = ''
+    for inst in instList:
+        try:
+            myInst = rm.open_resource(inst)
+            instID = myInst.query('*IDN?').split(',')
+            if 'KEITHLEY' in instID[0] and '2450' in instID[1]:
+                k2450Addr = inst
+            elif 'KEITHLEY' in instID[0] and '2700' in instID[1]:
+                k2700Addr = inst
+            elif 'TEKTRONIX' in instID[0] and 'AFG1022' in instID[1]:
+                AFG1022Addr = inst
+        except VisaIOError:
+            pass
+    try:
+        k2450 = Keithley2450(k2450Addr)
+        k2700 = Keithley2700(k2700Addr)
+        afg = AFG1022(AFG1022Addr)
+    except VisaIOError:
+        if test is True:
+            if not k2450Addr:
+                k2450 = FakeAdapter()
+            if not k2700Addr:
+                k2700 = FakeAdapter()
+            if not AFG1022Addr:
+                afg = FakeAdapter()
+        else:
+            # TODO: Prompt a GUI message instead
+            print("Instrument not connected! Check connections!")
+            exitprogram()
+    return k2450, k2700, afg
